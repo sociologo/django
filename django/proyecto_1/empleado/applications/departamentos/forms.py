505fda618624b30f1ff3d7369d2e845469ca36d9
Christian Castro <tarredwall@gmail.com>
2024-11-13 11:04:33 -0300

    up

--- django/proyecto_1/empleado/applications/departamentos/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms # type: ignore
-
-class NewDepartmentForm(forms.Form):
-    nombre = forms.CharField(max_length = 50)
-    apellido = forms.CharField(max_length = 50)
-    departamento = forms.CharField(max_length = 50)
-    shortname = forms.CharField(max_length = 20)
--- django/proyecto_1/empleado/applications/exp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Prueba
-
-admin.site.register(Prueba)
